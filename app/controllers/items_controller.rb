32d4b2138479a72673e18180d9f5f92857f0600f
Edmundo Figueroa <figuedmundo@gmail.com>
2012-04-19 01:25:45 -0400

    item index bien| show casi | update a medias |

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_filter :logear_usuario
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -9,11 +10,29 @@ class ItemsController < ApplicationController
     @item = cliente.items.build()
   end
 
+  def create
+    cliente = Usuario.find(session[:cliente])
+    @item = cliente.items.build(params[:item])
+    @item.estado = 'recibido'
+    if @item.save
+      flash[:notice] = "item creado"
+      redirect_to usuario_path(cliente)
+    else
+      flash.now[:error] = "#{params}"
+      render :new
+    end
+  end
+
   def show
     @item = Item.find_by_id(params[:id])
   end
 
+  def edit
+    @item = Item.find_by_id(params[:id])
+  end
+
   def update
+
   end
 
   def destroy
